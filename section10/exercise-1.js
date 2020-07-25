06985be489a1cc7ee13f4e6ae5ebe86976590ab1
Chandan <kchandan@thoughtworks.com>
2020-07-25 16:02:08 +0530

    JS stalker

---
+++ section10/exercise-1.js
@@ -0,0 +1,5 @@
+var firstname = prompt("What is your first name?")
+var lastname = prompt("What is your last name?")
+var age = prompt("What is your age?")
+console.log("Your full name is", firstname, lastname)
+console.log("You are", age, "years old")
