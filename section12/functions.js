b8778b8021bc21703892364bc6dbfa0299d097f3
Chandan <kchandan@thoughtworks.com>
2020-07-26 08:13:22 +0530

    Section 12 exercises

---
+++ section12/functions.js
@@ -0,0 +1,17 @@
+function isEven(x) {
+  return x % 2 === 0;
+}
+
+function factorial(x) {
+  var result = 1;
+  while (x > 1) {
+    result *= x;
+    x -= 1;
+  }
+  return result;
+}
+
+function kebabToSnake(term) {
+  // return term.split("-").join("_");
+  return term.replace(/-/g, "_");
+}
