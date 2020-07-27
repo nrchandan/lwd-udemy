39fdcd4967246f25ccc8baa08688ed69c7400209
Chandan <kchandan@thoughtworks.com>
2020-07-28 00:06:49 +0530

    Refactor code

--- section13/todo.js
+++ section13/todo.js
@@ -1,25 +1,37 @@
+var todos = []
 window.setTimeout(function () {
   var answer
-  var todos = []
   while (answer !== 'quit') {
     answer = window.prompt('What would you like todo?')
     if (answer === 'list') {
-      console.log('**********')
-      todos.forEach((todo, i) => {
-        console.log(i + ': ' + todo)
-      })
-      console.log('**********')
+      listTodos()
     } else if (answer === 'quit') {
       break
     } else if (answer === 'new') {
-      const todo = window.prompt('Enter a new todo')
-      todos.push(todo)
-      console.log(todo + ' added to list')
+      newTodo()
     } else if (answer === 'delete') {
-      const index = window.prompt('Enter index of todo to delete')
-      const removed = todos.splice(index, 1)
-      console.log('Todo ' + removed[0] + ' removed')
+      deleteTodo()
     }
   }
   console.log('Bye!')
 }, 500)
+
+function listTodos () {
+  console.log('**********')
+  todos.forEach((todo, i) => {
+    console.log(i + ': ' + todo)
+  })
+  console.log('**********')
+}
+
+function newTodo () {
+  const todo = window.prompt('Enter a new todo')
+  todos.push(todo)
+  console.log(todo + ' added to list')
+}
+
+function deleteTodo () {
+  const index = window.prompt('Enter index of todo to delete')
+  const removed = todos.splice(index, 1)
+  console.log('Todo ' + removed[0] + ' removed')
+}
