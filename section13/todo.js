93840f7b652ae4398de5d793c6c6e7e24d62eea8
Chandan <kchandan@thoughtworks.com>
2020-07-28 00:01:20 +0530

    Add delete & other stuff

--- section13/todo.js
+++ section13/todo.js
@@ -1,14 +1,25 @@
-window.setTimeout(function() {
-  var answer;
-  var todos = [];
-  while (answer !== "quit") {
-    answer = prompt("What would you like todo?")
-    if (answer === "list") {
-      console.log(todos);
-    } else if (answer === "quit") {
-      break;
-    } else if (answer === "new") {
-      todos.push(prompt("Enter a new todo"))
+window.setTimeout(function () {
+  var answer
+  var todos = []
+  while (answer !== 'quit') {
+    answer = window.prompt('What would you like todo?')
+    if (answer === 'list') {
+      console.log('**********')
+      todos.forEach((todo, i) => {
+        console.log(i + ': ' + todo)
+      })
+      console.log('**********')
+    } else if (answer === 'quit') {
+      break
+    } else if (answer === 'new') {
+      const todo = window.prompt('Enter a new todo')
+      todos.push(todo)
+      console.log(todo + ' added to list')
+    } else if (answer === 'delete') {
+      const index = window.prompt('Enter index of todo to delete')
+      const removed = todos.splice(index, 1)
+      console.log('Todo ' + removed[0] + ' removed')
     }
   }
+  console.log('Bye!')
 }, 500)
