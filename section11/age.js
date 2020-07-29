1025c70aa639155e8eb4bb2a4f88c8ce111bbebd
Chandan <kchandan@thoughtworks.com>
2020-07-29 10:23:07 +0530

    Follow consistent coding style

--- section11/age.js
+++ section11/age.js
@@ -1,14 +1,14 @@
-var age = prompt("Enter your age.")
+var age = window.prompt('Enter your age.')
 if (age < 0) {
-  console.log("Age cannot be negative.")
+  console.log('Age cannot be negative.')
 } else {
-  if (age == 21) {
-    console.log("Happy 21st birthday!!")
+  if (age === 21) {
+    console.log('Happy 21st birthday!!')
   }
   if (age % 2 === 1) {
-    console.log("Your age is odd!")
+    console.log('Your age is odd!')
   }
   if (age % Math.sqrt(age) === 0) {
-    console.log("Perfect square!")
+    console.log('Perfect square!')
   }
 }
